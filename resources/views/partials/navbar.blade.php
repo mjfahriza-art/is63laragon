05c87eeff3bb1b02ae1838824b0b93ebb834d86f
m_juhdi <mjfahriza@gmail.com>
2026-07-16 11:21:57 +0700

    clear

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,12 +1,42 @@
-{{-- Cek di resources/views/partials/navbar.blade.php --}}
-{{-- Bagian modal logout — pastikan action sudah benar --}}
- 
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog">
+{{-- resources/views/partials/navbar.blade.php
+     NOTE: Partial ini harus menyediakan tombol/trigger agar modal logout bisa dibuka.
+     Tombol/trigger ditaruh di kanan atas (gaya SB Admin 2 sederhana).
+--}}
+
+{{-- Topbar / Navbar --}}
+<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+    {{-- Sidebar Toggle (opsional) --}}
+    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3" type="button">
+        <i class="fa fa-bars"></i>
+    </button>
+
+    {{-- Right side --}}
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown no-arrow">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+                    {{ Auth::user()->name }}
+                </span>
+                <i class="fas fa-user-circle fa-fw text-gray-400"></i>
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Logout
+                </a>
+            </div>
+        </li>
+    </ul>
+</nav>
+
+{{-- Modal Logout --}}
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Konfirmasi Logout</h5>
-                <button class="close" type="button" data-dismiss="modal">
+                <h5 class="modal-title" id="logoutModalLabel">Konfirmasi Logout</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
@@ -14,8 +44,10 @@
                 Apakah Anda yakin ingin keluar dari sistem Simahaswa?
             </div>
             <div class="modal-footer">
-                <button class="btn btn-secondary" type="button"
-                        data-dismiss="modal">Batal</button>
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">
+                    Batal
+                </button>
+
                 {{-- Form POST ke route logout — @csrf wajib ada --}}
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
@@ -27,8 +59,4 @@
         </div>
     </div>
 </div>
- 
-{{-- Pastikan nama user tampil di dropdown --}}
-<span class="mr-2 d-none d-lg-inline text-gray-600 small">
-    {{ Auth::user()->name }}
-</span>
+
